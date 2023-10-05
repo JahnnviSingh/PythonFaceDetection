681a2a1713088e4f1682b54acdb8d2323168a4b8
JahnnviSingh <85466817+JahnnviSingh@users.noreply.github.com>
2023-10-05 12:23:42 +0530

    amritucode

--- mycode.py
+++ mycode.py
@@ -1,37 +1,38 @@
-import os
-import pickle
-import numpy as np
+mport os
 import cv2
+import pickle
 
-# Create a VideoCapture object to access the camera (0 for the default camera)
 import face_recognition
 
+# Create a VideoCapture object to access the camera (0 for the default camera)
 cap = cv2.VideoCapture(0)
 cap.set(3, 640)
 cap.set(4, 480)
 
-imgBackground = cv2.imread("ATTENDANCE SYSTEM.png")
-
+imgBackground = cv2.imread("ATTENDANCE SYSTEM.jpg")
 
 # Importing the mode images
 folderModePath = "Modes"
 modePath = os.listdir(folderModePath)
 imgModeList = []
 
-# THis will tell us if we have imported all the imges or not
+# This will tell us if we have imported all the imges or not
 for path in modePath:
     imgModeList.append(cv2.imread(os.path.join(folderModePath, path)))
 
 #print(len(imgModeList))
 
-#Load the encoding file
-print("Loading Encode File ...")
-file = open('EncodeFile.p', 'rb')
-encodeListKnownWithIDs = pickle.load(file)
+#load the encoding file
+
+print("Loading Encode File...")
+file = open('EncodeFile.p','rb')
+encodeListKnownWithIds = pickle.load(file)
 file.close()
-encodeListKnown, studentIds = encodeListKnownWithIDs
-print(studentIds)
-print("Encode File Loaded")
+encodeListKnown, studentNames = encodeListKnownWithIds
+#print(studentNames)
+print("Encode file loaded...")
+
+
 
 # Check if the camera opened successfully
 if not cap.isOpened():
@@ -47,33 +48,28 @@ while True:
         print("Error: Could not read frame.")
         break
 
-
-    imgS = cv2.resize(frame,(0,0), None, 0.25, 0.25)
+    imgS = cv2.resize(frame,(0,0),None, 0.25, 0.25)
     imgS = cv2.cvtColor(imgS, cv2.COLOR_BGR2RGB)
 
-    FaceCurFrame = face_recognition.face_locations(imgS)
-    encodeCurFrame = face_recognition.face_encodings(imgS, FaceCurFrame)
-
-
-    for encodeFace, faceLoc in zip(encodeCurFrame, FaceCurFrame):
-        matches = face_recognition.compare_faces(encodeListKnown, encodeFace)
-        faceDis = face_recognition.face_distance(encodeListKnown, encodeFace)
-        print("matches : ", matches)
-        print("faceDis : ", faceDis)
-
-        matchIndex = np.argmin(faceDis)
-        print("match Index : ", matchIndex)
-        
-        
-        
-        
+    faceCurrFrame = face_recognition.face_locations(imgS)
+    encodeCurrFrame = face_recognition.face_encodings(imgS, faceCurrFrame)
 
 
     imgBackground[162:162+480, 55:55+640] = frame
     imgBackground[113:113+535, 865:865 + 350] = imgModeList[0]
 
+    for encodeFace, faceLoc in zip(encodeCurrFrame, faceCurrFrame):
+        matches = face_recognition.compare_faces(encodeListKnown,encodeFace)
+        faceDis = face_recognition.face_distance(encodeListKnown,encodeFace) #the lower the face dist the better the match is
+        print("matches", matches)
+        print("faceDis", faceDis)
+
+
+
     #imgBackground[] = "Active.jpeg"
-    #imgBackground[] = "AlreadyMarked.jpeg"
+    #imgBackground[] = "Already Marked.jpeg"
+
+
 
     # Display the frame in a window
     #cv2.imshow('Camera', frame)
@@ -87,6 +83,3 @@ while True:
 # Release the camera and close the OpenCV window
 cap.release()
 cv2.destroyAllWindows()
-
-
-
